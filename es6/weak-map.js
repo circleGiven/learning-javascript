0536ee8a3cc8371b9d97049510006998846d1ea1
circleGiven <circlegiven@gmail.com>
2018-06-02 14:12:45 +0900

    weak map
    - test 코드 추가

--- es6/weak-map.js
+++ es6/weak-map.js
@@ -20,3 +20,29 @@ console.log(a.getSecret());
 b.setSecret('secret B');
 console.log(b.getSecret());
 // map과 weak map의 차이는 정확히 무엇인가?
+
+const SecretHolder2 = (function() {
+    const secrets = new WeakMap();
+    return class {
+        setSecret(secret) {
+            secrets.set(this, secret);
+        }
+
+        getSecret() {
+            return secrets.get(this);
+        }
+    }
+})();
+
+const c = new SecretHolder2();
+const test = {name: 'A'};
+c.setSecret(test);
+console.log(c.getSecret());
+test.name = 'B';
+console.log(c.getSecret());
+
+const d = new SecretHolder();
+d.setSecret(test);
+console.log(d.getSecret());
+test.name = 'A';
+console.log(d.getSecret());
