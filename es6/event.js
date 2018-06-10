adee9bafd551f101401bea1bd412565b13f749ea
circleGiven <circlegiven@gmail.com>
2018-06-10 14:35:31 +0900

    비동기 연습
    - 이벤트 패턴

---
+++ es6/event.js
@@ -0,0 +1,56 @@
+const EventEmitter = require('events').EventEmitter;
+
+class Countdown extends EventEmitter {
+    constructor(seconds, superstitious) {
+        super();
+        this.seconds = seconds;
+        this.superstitious = !!superstitious;
+    }
+
+    go() {
+        const countdown = this;
+        const timeoutIds = [];
+        return new Promise(function (resolve, reject) {
+            for(let i = countdown.seconds; i >= 0; i--) {
+                // 에러가 발생할 경우 취소
+                timeoutIds.push(
+                    setTimeout(function () {
+                        // 에러가 발생하면 대기중인 타임아웃을 취소
+                        if (countdown.superstitious && i === 13) {
+                            timeoutIds.forEach(clearTimeout);
+                            return reject(new Error("Oh my god"));
+                        }
+                        // tick 이벤트를 발생
+                        countdown.emit('tick', i);
+                        // 0일경우 성공
+                        if (i === 0) resolve();
+                    }, (countdown.seconds - i) * 1000)
+                );
+                // 에러가 발생할경우 표시만 해주고 그대로 나머지 진행
+                // setTimeout(function () {
+                //     // superstitious가 true이고 i가 13이면 reject
+                //     if (countdown.superstitious && i === 13) {
+                //         return reject(new Error("Oh my god"));
+                //     }
+                //     // tick 이벤트를 발생
+                //     countdown.emit('tick', i);
+                //     // 0일경우 성공
+                //     if (i === 0) resolve();
+                // }, (countdown.seconds - i) * 1000);
+            }
+        });
+    }
+}
+
+const c = new Countdown(16, true);
+// EventEmitter 의 on 메서드가 이벤트를 주시
+c.on('tick', function (i) {
+    if (i > 0) console.log(i + '...');
+});
+c.go()
+.then(function () {
+    console.log("GO!");
+})
+.catch(function (err) {
+   console.log(err.message);
+});
